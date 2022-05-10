14e8ed3f8443ab344d6ae4d49d5c0ecaf0556f4b
sonhyeono <oktlrp7624583@gmail.com>
2022-05-10 16:08:34 +0900

    Merge FrontEnd , BackEnd
    FrontEnd 에다가 BackEnd에 한 부분 이식했습니다.

--- main/views.py
+++ main/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import joblib
 
 def main(request):
 
     return render(request, 'frontend/main.html')
+
+
+def model_predict(texts):
+    transformer = joblib.load('main\model_folder\corpus_transformer.pkl')
+    model = joblib.load('main\model_folder\corpus.pkl')
+    ex = transformer.transform({texts})
+    y_pred = model.predict(ex)
+
+    return y_pred[0]
+    
+    
+
+def search_table(request):
+    search_key = request.GET.get('search_key') 
+    context = {'search_key': str(model_predict(search_key))} 
+
+    return JsonResponse(context)
